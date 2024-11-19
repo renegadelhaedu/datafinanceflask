d6dad55b02d252f022ce7840c325559b55a7b85b
KaueB15 <kauecardozo3@gmail.com>
2024-11-19 18:55:45 -0300

    feat: dinamic home response

--- static/js/home.js
+++ static/js/home.js
@@ -2,6 +2,11 @@ const loginButton = document.querySelector("#login-button")
 const loginContainer = document.querySelector("#login-container")
 const loginClose = document.querySelector("#login-close")
 
+const rankingButton = document.querySelector("#action-button")
+const mainIframe = document.querySelector("#iframe-main")
+const carrouselIframe = document.querySelector("#iframe-carousel")
+const cardIframe = document.querySelector("#iframe-card")
+
 
 loginButton.addEventListener("click", () => {
    loginContainer.classList.toggle("hidden")
@@ -10,6 +15,13 @@ loginButton.addEventListener("click", () => {
 
 loginClose.addEventListener("click", () => {
    loginContainer.classList.toggle("hidden")
+})
+
+rankingButton.addEventListener("click", () => {
+   mainIframe.src = "/rankingacoes/all"
+   mainIframe.classList.toggle("hidden")
+   carrouselIframe.classList.toggle("hidden")
+   cardIframe.classList.toggle("hidden")
 })
 
 
