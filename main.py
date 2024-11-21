87f2eaf15c1bfa9f2ff2ec5881aa601c407aa381
renegadelha <renegadelha@gmail.com>
2024-11-21 09:42:04 -0300

    add tabela de açoes na minha carteira - coloquei um blueprint na minhacarteira

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ import dataAnalise as da
 import grafico as gr
 import atualizar
 import dao 
-import carteira as gf
-import yfinance as yf
+
 
 
 app = Flask(__name__)
 app.secret_key = '6#aASD675@'
+
+from minhacarteira import  minhacarteira_bp
+
+
+app.register_blueprint(minhacarteira_bp, url_prefix="/acoes")
+
+
 
 @app.route('/')
 def home():
@@ -149,80 +155,6 @@ def gerariframecard():
     return render_template('cardActions.html', pares=pares)
 
 
-@app.route('/exibirdetalhesacao/<nome>', methods=['GET'])
-def exibir_detalhes_acao(nome):
-    tick = yf.Ticker(nome + '.SA')
-    info = tick.info
-
-    graf, valor_acao= gr.dados_acao(nome)
-    return render_template('dataActions.html', plot=graf, nome=nome, valor=round(valor_acao,2), info=info)
-
-
-@app.route('/acoes/adicionar', methods=['POST'])
-def inserir_acao():
-    if 'user' in session:
-        codigo = request.form.get('codigo') #falta verificar se o codigo da açao é valido
-        qtde = request.form.get('qtde')
-        pmedio = request.form.get('precomedio') #falta fazer tratamento ------------
-        email = session['user'][3]
-
-        if dao.inserir_acao(email, codigo, qtde, pmedio):
-            return redirect(url_for('gerarminhacarteira'))
-        else:
-            return '<h2>ação nao foi inserida</h2>' #criar pagina de erro ao inserir------------
-    else:
-        return render_template('home.html')
-
-
-@app.route('/acoes/atualizar', methods=['POST'])
-def atualizar_acao():
-    if 'user' in session and 'carteira' in session:
-        carteira_atualizada = dict()
-        for acao in session['carteira'].keys():
-            qtde_acao = int(request.form.get(acao))
-            if qtde_acao != session['carteira'].get(acao):
-                carteira_atualizada[acao] = qtde_acao
-
-        if len(carteira_atualizada) > 0:
-            if dao.atualizar_acoes(session['user'][3], carteira_atualizada):
-                return redirect(url_for('gerarminhacarteira'))
-            else:
-                return redirect(url_for('gerarminhacarteira'))
-        else:
-            return redirect(url_for('gerarminhacarteira'))
-
-
-@app.route('/acoes/paginas/<string:metodo>')
-def pagina_acoes_add(metodo):
-
-    if metodo == 'adicionar' and 'user' in session:
-        return render_template('adicionaracao.html')
-
-    elif metodo == 'atualizar' and 'user' in session:
-        carteira = dao.get_carteira(session['user'][3])
-        session['carteira'] = carteira
-        lista = [(chave, carteira.get(chave)) for chave in carteira.keys()]
-        return render_template('atualizaracao.html', acoes=lista)
-
-    elif metodo == 'excluir' and 'user' in session:
-        return render_template('adicionaracao.html') #---------falta fazer
-    else:
-        return render_template('minhacarteira.html')
-
-@app.route("/gerarminhacarteira")
-def gerarminhacarteira():
-    if 'user' in session:
-        data, grid = gf.gerarPercentuais(session['user'][3])
-        if data is None:
-            return render_template('adicionaracao.html')
-
-        lista = [['ticker', 'percentual']]
-        for key, val in data.items():
-            lista.append([key, val])
-
-        return render_template('mostrarcarteira.html', data=lista, grid=grid)
-    else:
-        return render_template('home.html')
 
 @app.route('/rankingdividendos/<opcao>', methods=['GET','POST'])
 def gerarrankingdividendos(opcao):
