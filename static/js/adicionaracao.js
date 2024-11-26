99801c2aba8cbb6938bd02e45cf93a55487200fb
KaueB15 <kauecardozo3@gmail.com>
2024-11-26 09:28:08 -0300

    feat: input accepts numbers

---
+++ static/js/adicionaracao.js
@@ -0,0 +1,17 @@
+const numberInput = document.querySelector("#number-input")
+const amountInput = document.querySelector("#amount-input")
+const priceInput = document.querySelector("#price-input")
+
+const arrayInputs = [numberInput, amountInput, priceInput]
+
+const validDigits = (text) => {
+    return text.replace(/[^0-9,]/g, "")
+}
+
+arrayInputs.forEach((input) => {
+    input.addEventListener("input", (e) => {
+        const updateValue = validDigits(e.target.value)
+
+        e.target.value = updateValue
+    })
+})
