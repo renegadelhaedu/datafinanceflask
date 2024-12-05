e790a37a9049432bf894ef4a110b785302405066
renegadelha <renegadelha@gmail.com>
2024-12-04 21:46:36 -0300

    método de filtragem melhorado com link front na homepageuser

--- filtragem_acoes/__init__.py
+++ filtragem_acoes/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, current_app, render_template, session, request, redirect, url_for
 import pandas as pd
-
+import dataAnalise as da
 
 
 filtragem_bp = Blueprint('filtragem', __name__)
@@ -9,7 +9,11 @@ filtragem_bp = Blueprint('filtragem', __name__)
 def exibir_acoes_filtradas():
     dados = pd.read_pickle('data/filtragem_acoes.pkl')
 
-    table_html = dados.to_html(classes="table table-striped", index=False, border=0)
+    dados.drop(dados[dados['margemGordon'] < -100].index, inplace=True)
+    dados = da.computarRankingFundamentos(dados)
+    dados.sort_values(by=['notaMedia'], ascending=False, inplace=True)
+
+    table_html = dados.to_html(classes="table table-striped", index=True, border=1)
     return render_template("filtragemacoes.html", table=table_html)
 
 
