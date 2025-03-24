fb5312eda745227973a89d404f981330f149bb44
renegadelhaedu <renegadelha.edu@gmail.com>
2025-03-24 12:16:07 -0300

    ajuste da coluna Adj Close - Close do yfinance

--- riskfinancetools/betaIndicator_script.py
+++ riskfinancetools/betaIndicator_script.py
@@ -3,11 +3,11 @@ import numpy as np
 def betaIndicator(dataStock, dataMarket, logReturns=True):
 
     if not logReturns:
-      dataStock['RETURNS'] = dataStock['Adj Close'].pct_change()
-      dataMarket['RETURNS'] = dataMarket['Adj Close'].pct_change()
+      dataStock['RETURNS'] = dataStock['Close'].pct_change()
+      dataMarket['RETURNS'] = dataMarket['Close'].pct_change()
     else:
-      dataStock['RETURNS'] = np.log(dataStock['Adj Close'] / dataStock['Adj Close'].shift(1))
-      dataMarket['RETURNS'] = np.log(dataMarket['Adj Close'] / dataMarket['Adj Close'].shift(1))
+      dataStock['RETURNS'] = np.log(dataStock['Close'] / dataStock['Close'].shift(1))
+      dataMarket['RETURNS'] = np.log(dataMarket['Close'] / dataMarket['Close'].shift(1))
 
     stock_returns, market_returns = dataStock.align(dataMarket, join='inner')
 
--- riskfinancetools/std_script.py
+++ riskfinancetools/std_script.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def standardDerivation(data):
-  data['returns'] = data['Adj Close'].pct_change()
+  data['returns'] = data['Close'].pct_change()
   stdTickerStock = np.std(data['returns'])
 
   return stdTickerStock
